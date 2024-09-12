973d4c5d6c3bad6201794afd4076c30c04419d10
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-12 15:41:19 +0400

    added logo

---
+++ features/editor/components/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLink to="/">
+    <div class="size-8 relative shrink-0">
+      <NuxtImg
+        src="/logo.svg"
+        fill
+        alt="Image AI"
+        classs="shrink-0 hover:opacity-75 transition"
+      />
+    </div>
+  </NuxtLink>
+</template>
