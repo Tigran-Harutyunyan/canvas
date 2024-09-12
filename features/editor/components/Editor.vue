17d5b8e80c28df83cd3d93bc06cb48181b33a8b4
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-12 14:23:03 +0400

    canvas is not friendly with proxy

--- features/editor/components/Editor.vue
+++ features/editor/components/Editor.vue
@@ -1,7 +1,11 @@
 <script lang="ts" setup>
 import { fabric } from "fabric";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import { useEditor } from "@/features/editor/composables/useEditor";
+import Navbar from "./Navbar.vue";
+import Sidebar from "./Sidebar.vue";
+import Toolbar from "./Toolbar.vue";
+import Footer from "./Footer.vue";
 
 interface EditorProjectIdPageProps {
   params: {
@@ -14,30 +18,40 @@ const containerRef = ref(null);
 
 const { init } = useEditor();
 
-let canvas = ref();
+let canvas: fabric.Canvas | null = null;
 
 onMounted(() => {
   if (canvasRef.value && containerRef.value) {
-    canvas.value = new fabric.Canvas(canvasRef.value, {
+    canvas = new fabric.Canvas(canvasRef.value, {
       controlsAboveOverlay: true,
       preserveObjectStacking: true,
     });
 
     init({
-      initialCanvas: canvas.value,
+      initialCanvas: canvas,
       initialContainer: containerRef.value,
     });
+  }
+});
+
+onUnmounted(() => {
+  if (canvas) {
+    canvas?.dispose();
   }
 });
 </script>
 
 <template>
   <div class="h-full flex flex-col">
+    <Navbar />
     <div class="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+      <Sidebar />
       <main class="bg-muted flex-1 overflow-auto relative flex flex-col">
+        <Toolbar />
         <div class="flex-1 h-[calc(100%-124px)] bg-muted" ref="containerRef">
           <canvas ref="canvasRef" />
         </div>
+        <Footer />
       </main>
     </div>
   </div>
