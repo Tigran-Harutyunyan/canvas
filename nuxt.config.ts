b08837ada953e1ccaf8d77764d51bfca48c0f777
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-12 12:42:46 +0400

    project initiation

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  runtimeConfig: {
+    public: {
+      // clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+      // cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+      // cloudApiKey: process.env.CLOUDINARY_API_KEY,
+      // appUrl: process.env.APP_URL,
+    }
+  },
+})
