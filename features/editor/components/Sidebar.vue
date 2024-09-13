f8e8c15e13e52ddfdce2ac5e0229040b68704427
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-13 20:03:03 +0400

    added sidebar and sidebarItem component

--- features/editor/components/Sidebar.vue
+++ features/editor/components/Sidebar.vue
@@ -1,6 +1,101 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  LayoutTemplate,
+  ImageIcon,
+  Pencil,
+  Settings,
+  Shapes,
+  Sparkles,
+  Type,
+} from "lucide-vue-next";
+
+import { type ActiveTool } from "@/features/editor/types";
+import SidebarItem from "@/features/editor/components/SidebarItem.vue";
+
+interface SidebarProps {
+  activeTool: ActiveTool;
+}
+
+const props = defineProps<SidebarProps>();
+
+const emit = defineEmits<{
+  (e: "onChangeActiveTool", tool: ActiveTool): void;
+}>();
+</script>
 <template>
   <aside
     class="bg-white flex flex-col w-[100px] h-full border-r overflow-y-auto"
-  ></aside>
+  >
+    <ul class="flex flex-col">
+      <SidebarItem
+        label="Design"
+        :isActive="props.activeTool === 'templates'"
+        @click="emit('onChangeActiveTool', 'templates')"
+      >
+        <template #icon>
+          <LayoutTemplate />
+        </template>
+      </SidebarItem>
+
+      <SidebarItem
+        label="Image"
+        :isActive="props.activeTool === 'images'"
+        @click="emit('onChangeActiveTool', 'images')"
+      >
+        <template #icon>
+          <ImageIcon />
+        </template>
+      </SidebarItem>
+
+      <SidebarItem
+        label="Text"
+        :isActive="props.activeTool === 'text'"
+        @click="emit('onChangeActiveTool', 'text')"
+      >
+        <template #icon>
+          <Type />
+        </template>
+      </SidebarItem>
+
+      <SidebarItem
+        label="Shapes"
+        :isActive="props.activeTool === 'shapes'"
+        @click="emit('onChangeActiveTool', 'shapes')"
+      >
+        <template #icon>
+          <Shapes />
+        </template>
+      </SidebarItem>
+
+      <SidebarItem
+        label="Draw"
+        :isActive="props.activeTool === 'draw'"
+        @click="emit('onChangeActiveTool', 'draw')"
+      >
+        <template #icon>
+          <Pencil />
+        </template>
+      </SidebarItem>
+
+      <SidebarItem
+        label="AI"
+        :isActive="props.activeTool === 'ai'"
+        @click="emit('onChangeActiveTool', 'ai')"
+      >
+        <template #icon>
+          <Sparkles />
+        </template>
+      </SidebarItem>
+
+      <SidebarItem
+        label="Settings"
+        :isActive="props.activeTool === 'settings'"
+        @click="emit('onChangeActiveTool', 'settings')"
+      >
+        <template #icon>
+          <Settings />
+        </template>
+      </SidebarItem>
+    </ul>
+  </aside>
 </template>
---
+++ features/editor/components/SidebarItem.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+interface SidebarItemProps {
+  label: string;
+  isActive?: boolean;
+}
+
+const props = withDefaults(defineProps<SidebarItemProps>(), {
+  isActive: false,
+});
+
+const emit = defineEmits<{
+  (e: "click"): void;
+}>();
+</script>
+<template>
+  <Button
+    variant="ghost"
+    @click="emit('click')"
+    :class="
+      cn(
+        'w-full h-full aspect-video p-3 py-4 flex flex-col rounded-none',
+        props.isActive && 'bg-muted text-primary'
+      )
+    "
+  >
+    <div class="size-5 stroke-2 shrink-0">
+      <slot name="icon" />
+    </div>
+    <span class="mt-2 text-xs"> {{ label }} </span>
+  </Button>
+</template>
