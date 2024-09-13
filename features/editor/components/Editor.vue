a372e87116f61344fcce7d8c618dccfba56b0af2
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-13 20:05:25 +0400

    Updated Editor root component

--- features/editor/components/Editor.vue
+++ features/editor/components/Editor.vue
@@ -4,21 +4,50 @@ import { ref, onMounted, onUnmounted } from "vue";
 import { useEditor } from "@/features/editor/composables/useEditor";
 import Navbar from "./Navbar.vue";
 import Sidebar from "./Sidebar.vue";
+import ShapeSidebar from "./ShapeSidebar.vue";
+import SettingsSidebar from "./SettingsSidebar.vue";
 import Toolbar from "./Toolbar.vue";
 import Footer from "./Footer.vue";
+import type { ActiveTool, Editor } from "@/features/editor/types";
+//import { type ResponseType } from "@/features/projects/api/use-get-project";
 
-interface EditorProjectIdPageProps {
-  params: {
-    projectId: string;
-  };
+interface EditorProps {
+  // initialData: ResponseType["data"];
+  initialData: ResponseType["default"];
 }
+
+const props = defineProps<EditorProps>();
 
 const canvasRef = ref(null);
 const containerRef = ref(null);
 
-const { init } = useEditor();
+const activeTool = ref<ActiveTool>("select");
+
+const { init, editor } = useEditor({
+  defaultState: props.initialData?.json,
+  defaultWidth: props.initialData?.width,
+  defaultHeight: props.initialData?.height,
+  // clearSelectionCallback: onClearSelection,
+  // saveCallback: debouncedSave,
+});
 
 let canvas: fabric.Canvas | null = null;
+
+const onChangeActiveTool = (tool: ActiveTool) => {
+  if (tool === "draw") {
+    // editor?.enableDrawingMode();
+  }
+
+  if (activeTool.value === "draw") {
+    // editor?.disableDrawingMode();
+  }
+
+  if (tool === activeTool.value) {
+    return (activeTool.value = "select");
+  }
+
+  activeTool.value = tool;
+};
 
 onMounted(() => {
   if (canvasRef.value && containerRef.value) {
@@ -43,11 +72,26 @@ onUnmounted(() => {
 
 <template>
   <div class="h-full flex flex-col">
-    <Navbar />
+    <Navbar :activeTool="activeTool" @onChangeActiveTool="onChangeActiveTool" />
     <div class="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
-      <Sidebar />
+      <Sidebar
+        :activeTool="activeTool"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <ShapeSidebar
+        :activeTool="activeTool"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <SettingsSidebar
+        :activeTool="activeTool"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
       <main class="bg-muted flex-1 overflow-auto relative flex flex-col">
-        <Toolbar />
+        <Toolbar
+          :activeTool="activeTool"
+          @onChangeActiveTool="onChangeActiveTool"
+        />
         <div class="flex-1 h-[calc(100%-124px)] bg-muted" ref="containerRef">
           <canvas ref="canvasRef" />
         </div>
