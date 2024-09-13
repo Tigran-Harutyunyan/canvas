b6e5d6bbd8c0a22f66ecbe6b4dab3a2cf1f3510f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-13 20:07:54 +0400

    created Footer component

--- features/editor/components/Footer.vue
+++ features/editor/components/Footer.vue
@@ -1,6 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Minimize, ZoomIn, ZoomOut } from "lucide-vue-next";
+
+import { type Editor } from "@/features/editor/types";
+
+import Hint from "@/components/Hint.vue";
+import { Button } from "@/components/ui/button";
+
+interface FooterProps {
+  editor: Editor | undefined;
+}
+
+const props = defineProps<FooterProps>();
+</script>
 <template>
   <footer
     class="h-[52px] border-t bg-white w-full flex items-center overflow-x-auto z-[49] p-2 gap-x-1 shrink-0 px-4 flex-row-reverse"
-  ></footer>
+  >
+    <Hint label="Reset" side="top" :sideOffset="10">
+      <Button
+        @click="props.editor?.autoZoom()"
+        size="icon"
+        variant="ghost"
+        class="h-full"
+      >
+        <Minimize class="size-4" />
+      </Button>
+    </Hint>
+    <Hint label="Zoom in" side="top" :sideOffset="10">
+      <Button
+        @click="props.editor?.zoomIn()"
+        size="icon"
+        variant="ghost"
+        class="h-full"
+      >
+        <ZoomIn class="size-4" />
+      </Button>
+    </Hint>
+    <Hint label="Zoom out" side="top" :sideOffset="10">
+      <Button
+        @click="props.editor?.zoomOut()"
+        size="icon"
+        variant="ghost"
+        class="h-full"
+      >
+        <ZoomOut class="size-4" />
+      </Button>
+    </Hint>
+  </footer>
 </template>
