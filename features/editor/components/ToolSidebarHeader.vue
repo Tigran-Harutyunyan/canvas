1150245aeb26bb4e8b7c72f1bdff53bfa1077b8b
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-15 09:13:09 +0400

    created tool sidebar header component

---
+++ features/editor/components/ToolSidebarHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface ToolSidebarHeaderProps {
+  title: string;
+  description?: string;
+}
+
+const props = defineProps<ToolSidebarHeaderProps>();
+</script>
+
+<template>
+  <div class="p-4 border-b space-y-1 h-[68px]">
+    <p class="text-sm font-medium">
+      {{ $props.title }}
+    </p>
+
+    <p v-if="props.description" class="text-xs text-muted-foreground">
+      {{ props.description }}
+    </p>
+  </div>
+</template>
