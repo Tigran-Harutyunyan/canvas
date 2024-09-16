0db3e8e80a00cd104a2d6bb09ee9962101e77eef
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-16 21:40:31 +0400

    updated Editor.vue file

--- features/editor/components/Editor.vue
+++ features/editor/components/Editor.vue
@@ -6,9 +6,23 @@ import Navbar from "./Navbar.vue";
 import Sidebar from "./Sidebar.vue";
 import ShapeSidebar from "./ShapeSidebar.vue";
 import SettingsSidebar from "./SettingsSidebar.vue";
+import FillColorSidebar from "./FillColorSidebar.vue";
+import StrokeColorSidebar from "./StrokeColorSidebar.vue";
+import StrokeWidthSidebar from "./StrokeWidthSidebar.vue";
+import OpacitySidebar from "./OpacitySidebar.vue";
+import TextSidebar from "./TextSidebar.vue";
+import FontSidebar from "./FontSidebar.vue";
+import DrawSidebar from "./DrawSidebar.vue";
+import FilterSidebar from "./FilterSidebar.vue";
+import ImageSidebar from "./ImageSidebar.vue";
+import TemplateSidebar from "./TemplateSidebar.vue";
 import Toolbar from "./Toolbar.vue";
 import Footer from "./Footer.vue";
-import type { ActiveTool, Editor } from "@/features/editor/types";
+import {
+  type ActiveTool,
+  type Editor,
+  activeToolValues,
+} from "@/features/editor/types";
 //import { type ResponseType } from "@/features/projects/api/use-get-project";
 
 interface EditorProps {
@@ -34,12 +48,12 @@ const { init, editor } = useEditor({
 let canvas: fabric.Canvas | null = null;
 
 const onChangeActiveTool = (tool: ActiveTool) => {
-  if (tool === "draw") {
-    // editor?.enableDrawingMode();
+  if (tool === activeToolValues.DRAW) {
+    editor.value?.enableDrawingMode();
   }
 
-  if (activeTool.value === "draw") {
-    // editor?.disableDrawingMode();
+  if (activeTool.value === activeToolValues.DRAW) {
+    editor.value?.disableDrawingMode();
   }
 
   if (tool === activeTool.value) {
@@ -62,7 +76,6 @@ onMounted(() => {
     });
   }
 });
-
 onUnmounted(() => {
   if (canvas) {
     canvas?.dispose();
@@ -72,30 +85,87 @@ onUnmounted(() => {
 
 <template>
   <div class="h-full flex flex-col">
-    <Navbar :activeTool="activeTool" @onChangeActiveTool="onChangeActiveTool" />
+    <Navbar
+      :activeTool="activeTool"
+      :editor="editor"
+      @onChangeActiveTool="onChangeActiveTool"
+    />
     <div class="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
       <Sidebar
         :activeTool="activeTool"
         @onChangeActiveTool="onChangeActiveTool"
       />
+      <TemplateSidebar
+        v-if="activeTool === activeToolValues.TEMPLATES"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
       <ShapeSidebar
-        :activeTool="activeTool"
+        v-if="activeTool === activeToolValues.SHAPES"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <FillColorSidebar
+        v-if="activeTool === activeToolValues.FILL"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <StrokeColorSidebar
+        v-if="activeTool === activeToolValues.STROKE_COLOR"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <StrokeWidthSidebar
+        v-if="activeTool === activeToolValues.STROKE_WIDTH"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <OpacitySidebar
+        v-if="activeTool === activeToolValues.OPACITY"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <TextSidebar
+        v-if="activeTool === activeToolValues.TEXT"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <FontSidebar
+        v-if="activeTool === activeToolValues.FONT"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <DrawSidebar
+        v-if="activeTool === activeToolValues.DRAW"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <FilterSidebar
+        v-if="activeTool === activeToolValues.FILTER"
+        :editor="editor"
+        @onChangeActiveTool="onChangeActiveTool"
+      />
+      <ImageSidebar
+        v-show="activeTool === activeToolValues.IMAGES"
         :editor="editor"
         @onChangeActiveTool="onChangeActiveTool"
       />
       <SettingsSidebar
-        :activeTool="activeTool"
+        v-if="activeTool === activeToolValues.SETTINGS"
+        :editor="editor"
         @onChangeActiveTool="onChangeActiveTool"
       />
       <main class="bg-muted flex-1 overflow-auto relative flex flex-col">
         <Toolbar
+          v-if="editor"
           :activeTool="activeTool"
+          :editor="editor"
           @onChangeActiveTool="onChangeActiveTool"
         />
         <div class="flex-1 h-[calc(100%-124px)] bg-muted" ref="containerRef">
           <canvas ref="canvasRef" />
         </div>
-        <Footer />
+        <Footer :editor="editor" />
       </main>
     </div>
   </div>
