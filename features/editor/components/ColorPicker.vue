5762d4ed294b22d618e9bdfa2d81c345fda76aba
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-16 12:48:02 +0400

    updated color picker with custom colors

--- features/editor/components/ColorPicker.vue
+++ features/editor/components/ColorPicker.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { ColorPicker } from "vue3-colorpicker";
 import "vue3-colorpicker/style.css";
+import { colors } from "@/features/editor/types";
 
 interface ColorPickerProps {
   value: string;
 }
+
+const pureColor = ref();
 
 const props = defineProps<ColorPickerProps>();
 const emit = defineEmits<{
@@ -14,16 +17,45 @@ const emit = defineEmits<{
 const onUpdatePureColor = (color: string) => {
   emit("onChange", color);
 };
+
+const onChooseColor = (color: string) => {
+  const hex_color = color.replace("#", ""),
+    r = parseInt(hex_color.substring(0, 2), 16),
+    g = parseInt(hex_color.substring(2, 4), 16),
+    b = parseInt(hex_color.substring(4, 6), 16);
+  const rgb = "rgb(" + r + "," + g + "," + b + ")";
+  emit("onChange", rgb);
+  pureColor.value = rgb;
+};
+
+watch(
+  () => props.value,
+  () => {
+    pureColor.value = props.value;
+  },
+  {
+    immediate: true,
+  }
+);
 </script>
 
 <template>
   <div class="w-full space-y-4">
     <ColorPicker
       :isWidget="true"
-      :pureColor="props.value"
+      :pureColor="pureColor"
       pickerType="chrome"
       class="border rounded-lg"
       @update:pureColor="onUpdatePureColor"
     />
+
+    <ul class="flex flex-wrap gap-2 pr-5">
+      <li
+        v-for="color in colors"
+        @click="onChooseColor(color)"
+        class="w-6 h-6 rounded-full cursor-pointer"
+        :style="{ backgroundColor: color }"
+      ></li>
+    </ul>
   </div>
 </template>
