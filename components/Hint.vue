3a6fbdd5c098b110dc293940ddd1877e5a90b56c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-17 12:55:09 +0400

    created Hint component as a wrapper for tooltip

---
+++ components/Hint.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export interface HintProps {
+  label: string;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
+}
+
+defineProps<HintProps>();
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip :delayDuration="100">
+      <TooltipTrigger as-child>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent
+        class="text-white bg-slate-800 border-slate-800"
+        :side="side"
+        :align="align"
+        :sideOffset="sideOffset"
+        :alignOffset="alignOffset"
+      >
+        <p class="font-semibold capitalize">
+          {{ label }}
+        </p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
