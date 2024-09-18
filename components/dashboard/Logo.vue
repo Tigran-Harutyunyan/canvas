4b64040ff7e89aacd6d3121aa518e657a1c9abc7
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-18 15:47:13 +0400

    created Logo component for dashboard

---
+++ components/dashboard/Logo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <NuxtLink to="/">
+    <div
+      class="flex items-center gap-x-2 hover:opacity-75 transition h-[68px] px-4"
+    >
+      <div class="size-8 relative">
+        <NuxtImg src="/logo.svg" alt="Image AI" fill />
+      </div>
+      <h1 class="text-xl font-bold">Image AI</h1>
+    </div>
+  </NuxtLink>
+</template>
