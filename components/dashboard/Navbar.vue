c317f25b11445c867cf58ac1b5739fa146b82699
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-18 15:47:35 +0400

    created Navbar component for dashboard

---
+++ components/dashboard/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { UserButton, SignedIn } from "vue-clerk";
+</script>
+
+<template>
+  <nav class="w-full flex items-center p-4 h-[68px]">
+    <div class="ml-auto">
+      <UserButton v-if="SignedIn" />
+    </div>
+  </nav>
+</template>
