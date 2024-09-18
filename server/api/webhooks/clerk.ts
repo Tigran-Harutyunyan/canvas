5cc93e76a8f0d799f7623261cebdf7bf253e7193
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-18 11:54:36 +0400

    created webhook for clerk to hook user creation

---
+++ server/api/webhooks/clerk.ts
@@ -0,0 +1,51 @@
+import { eq } from "drizzle-orm";
+import { db } from "@/db/drizzle";
+import { users } from "@/db/schema";
+
+export default defineEventHandler(async (event) => {
+
+    const { data, type: eventType } = await readBody(event);
+
+    const { id, email_addresses, first_name } = data;
+
+    if (!id) {
+        setResponseStatus(event, 403)
+        return ''
+    }
+
+    if (eventType === 'user.created') {
+        try {
+            const emailAddress = email_addresses?.[0]?.email_address;
+
+            const query = await db
+                .select()
+                .from(users)
+                .where(eq(users.email, emailAddress));
+
+            if (query[0]) {
+                return createError({
+                    statusCode: 400,
+                    statusMessage: 'Email already in use'
+                })
+            }
+
+            const response = await db.insert(users).values({
+                email: emailAddress,
+                name: first_name || ''
+            });
+
+            console.log('response', response);
+
+            return { success: true }
+
+
+        } catch (error) {
+            return createError({
+                statusCode: 500,
+                statusMessage: error?.message
+            })
+        }
+    }
+
+
+});
