15158cf9d8217caceeb360b0b5c0ce6665c59839
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-18 15:46:46 +0400

    created sidebar and sidebar item components for dashboard

---
+++ components/dashboard/Sidebar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Logo from "./Logo.vue";
+import SidebarRoutes from "./SidebarRoutes.vue";
+</script>
+
+<template>
+  <aside class="hidden lg:flex fixed flex-col w-[300px] left-0 shrink-0 h-full">
+    <Logo />
+    <SidebarRoutes />
+  </aside>
+</template>
---
+++ components/dashboard/SidebarItem.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+
+interface SidebarItemProps {
+  label: string;
+  href: string;
+  isActive?: boolean;
+}
+
+defineProps<SidebarItemProps>();
+
+const onClick = () => {};
+</script>
+
+<template>
+  <NuxtLink :to="href" @onClick="onClick">
+    <div
+      :class="
+        cn(
+          'flex items-center px-3 py-3 rounded-xl bg-transparent hover:bg-white transition',
+          isActive && 'bg-white'
+        )
+      "
+    >
+      <div class="size-4 mr-4 stroke-2"><slot /></div>
+      <span class="text-sm font-medium mt-2">
+        {{ label }}
+      </span>
+    </div>
+  </NuxtLink>
+</template>
