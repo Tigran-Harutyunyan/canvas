02e1afdaeeee68911be3171cad0a052428b3f42d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-18 11:54:03 +0400

    added auth pages

---
+++ pages/sign-in.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { SignIn } from "vue-clerk";
+useHead({
+  title: "Sign-in",
+});
+</script>
+
+<template>
+  <NuxtLayout name="auth">
+    <div class="grid h-full place-items-center">
+      <SignIn sign-up-url="/sign-up" afterSignInUrl="/" />
+    </div>
+  </NuxtLayout>
+</template>
---
+++ pages/sign-up.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { SignUp } from "vue-clerk";
+useHead({
+  title: "Sign up",
+});
+</script>
+
+<template>
+  <NuxtLayout name="auth">
+    <div class="grid h-full place-items-center">
+      <SignUp sign-in-url="/sign-in" afterSignUpUrl="/" />
+    </div>
+  </NuxtLayout>
+</template>
