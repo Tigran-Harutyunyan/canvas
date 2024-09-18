a0e7723f83b6b1d885d79678e33c9a03a5526fb6
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-18 15:49:49 +0400

    created layouts for project

---
+++ layouts/auth.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-[url(/bg.jpg)] bg-top bg-cover h-full flex flex-col">
+    <div class="z-[4] h-full w-full flex flex-col items-center justify-center">
+      <div class="h-full w-full md:h-auto md:w-[420px]">
+        <slot />
+      </div>
+    </div>
+    <div
+      class="fixed inset-0 bg-[linear-gradient(180deg,rgba(0,0,0,0.8),rgba(0,0,0,.4),rgba(0,0,0,.8))] z-[1]"
+    />
+  </div>
+</template>
---
+++ layouts/dashboard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Navbar from "../components/dashboard/Navbar.vue";
+import Sidebar from "../components/dashboard/Sidebar.vue";
+</script>
+
+<template>
+  <div class="bg-muted h-full">
+    <Sidebar />
+    <div class="lg:pl-[300px] flex flex-col h-full">
+      <Navbar />
+      <main class="bg-white flex-1 overflow-auto p-8 lg:rounded-tl-2xl">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
