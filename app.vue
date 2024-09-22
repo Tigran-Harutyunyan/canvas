1339a6527ff95441afc139ee8951ff20c5ba69c6
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-22 10:20:29 +0400

    added modals and toaster

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ClerkLoaded } from "vue-clerk";
+import Toaster from "@/components/ui/toast/Toaster.vue";
+import Modals from "@/components/Modals.vue";
+</script>
 <template>
-  <NuxtLayout>
+  <ClerkLoaded>
+    <Modals />
+    <Toaster class="pointer-events-auto" />
     <NuxtPage />
-  </NuxtLayout>
+  </ClerkLoaded>
 </template>
