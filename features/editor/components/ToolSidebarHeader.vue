be7675a777097ec2e20ef1223d453b9528c93ab1
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-22 11:52:16 +0400

    misc

--- features/editor/components/ToolSidebarHeader.vue
+++ features/editor/components/ToolSidebarHeader.vue
@@ -4,17 +4,17 @@ interface ToolSidebarHeaderProps {
   description?: string;
 }
 
-const props = defineProps<ToolSidebarHeaderProps>();
+defineProps<ToolSidebarHeaderProps>();
 </script>
 
 <template>
   <div class="p-4 border-b space-y-1 h-[68px]">
     <p class="text-sm font-medium">
-      {{ $props.title }}
+      {{ title }}
     </p>
 
-    <p v-if="props.description" class="text-xs text-muted-foreground">
-      {{ props.description }}
+    <p v-if="description" class="text-xs text-muted-foreground">
+      {{ description }}
     </p>
   </div>
 </template>
