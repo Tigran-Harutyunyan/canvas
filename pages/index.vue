6d2627296356c4d68dc03ac14551b7778345d58a
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-22 10:26:50 +0400

    created index page

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Banner from "@/components/dashboard/Banner.vue";
+import TemplatesSection from "@/components/dashboard/TemplatesSection.vue";
+import ProjectsSection from "@/components/dashboard/ProjectsSection.vue";
+</script>
+<template>
+  <NuxtLayout name="dashboard">
+    <div class="flex flex-col space-y-6 max-w-screen-xl mx-auto pb-10">
+      <Banner />
+      <TemplatesSection />
+      <ProjectsSection />
+    </div>
+  </NuxtLayout>
+</template>
