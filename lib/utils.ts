49178808c48a5e99acab15d2f4d4782dce19ea31
Tigran <harutyunyan.tigran1975@gmail.com>
2024-09-22 10:28:45 +0400

    created utils file

---
+++ lib/utils.ts
@@ -0,0 +1,20 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const formatPrice = (price: number) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
+
+  return formatter.format(price)
+}
+
+export const formatDate = (date: string) => {
+  const newDate = new Date(date)
+  return newDate.toLocaleDateString();
+} 
